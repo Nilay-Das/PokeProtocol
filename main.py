4639955855465e7f68864714defdeec69b94cf42
Shoofy32 <stefandomingo17@gmail.com>
2025-11-29 22:44:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,27 @@
 from protocol.battle_state import Move, BattleState, calculate_damage, apply_damage
 from protocol.pokemon_db import load_pokemon_db
 
+import host
+import joiner
+
 print("--- Pokemon Battle Simulator ---")
 
 # Load the database
 db = load_pokemon_db()
 
-# Get two Pokemon
-p1 = db["charmander"]
-p2 = db["bulbasaur"]
+host = host.host()
+joiner = joiner.joiner()
+#spectator = Spectator.Spectator()
+print("h for host, j for joiner, s for spectator(not yet implemented)")
+print("For host, please just use port 5001 this is a test for now")
+choice = input()
 
-print("Player 1: " + p1.name)
-print("Player 2: " + p2.name)
-
-# Show moves
-print("Charmander's moves: " + str(p1.moves))
-
-# Make a move
-# We will use the first move for now
-move_name = "Basic Attack"
-if len(p1.moves) > 0:
-    move_name = p1.moves[0]
-    
-print("Charmander chooses: " + move_name)
-
-# Create the move object
-# Let's assume it is a special move and type is fire
-move = Move(move_name, 70, "special", "fire")
-
-# Create battle state
-state = BattleState(p1, p2)
-
-print("\n--- Battle Start ---")
-print(p2.name + " has " + str(p2.current_hp) + " HP.")
-
-# Calculate damage
-dmg = calculate_damage(state, move)
-print("Calculated damage: " + str(dmg))
-
-# Apply it
-apply_damage(state, dmg)
+if choice == "h":
+    host.accept()
+if choice == "j":
+    joiner.start("127.0.0.1", 5001)
+if choice == "h":
+    sepctator.start("127.0.0.1", 5001)
 
 print(p2.name + " now has " + str(p2.current_hp) + " HP.")
 print("--- Battle End ---")
